17a198e2f0112f2dfc1bd02fe137e0b4f543a603
sricharan12-hub <sricharanpalem07@gmail.com>
2025-09-13 11:37:09 +0530

    Fixed Signup.css import issue

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './signup.css'
+import './Signup.css'
 
 function Signup() {
   const navigate = useNavigate();
